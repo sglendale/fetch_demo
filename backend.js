75ac566b6fcc27158146e6ab389bb826719717ce
unknown <sglen001@fiu.edu>
2022-12-04 16:10:54 -0500

    Responds to populate published and populate saved

--- backend.js
+++ backend.js
@@ -17,6 +17,39 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
+app.get('/users/10001/courses', (req, res) => {
+  var readStream = fs.readFileSync('./private/user_courses.json');
+  res.status(200);
+  res.send(readStream);
+});
+
+app.get('/course/50001/user/10001/savedNotes', (req, res) => {
+  var readStream = fs.readFileSync('./private/saved_notes.json');
+  res.status(200);
+  res.send(readStream);
+});
+
+/* This will result in an empty set */
+app.get('/course/50002/user/10001/savedNotes', (req, res) => {
+  var readStream = fs.readFileSync('./private/empty_notes.json');
+  res.status(200);
+  res.send(readStream);
+});
+
+/* This will result in an empty set */
+app.get('/course/50002/user/10001/submittedNotes', (req, res) => {
+  var readStream = fs.readFileSync('./private/submitted_notes.json');
+  res.status(200);
+  res.send(readStream);
+});
+
+/* This will result in an empty set */
+app.get('/course/50001/user/10001/submittedNotes', (req, res) => {
+  var readStream = fs.readFileSync('./private/empty_notes.json');
+  res.status(200);
+  res.send(readStream);
+});
+
 app.post('/register', (req, res) => {
     var readStream = fs.readFileSync('./private/succesful_registration.json');
     res.status(200);
