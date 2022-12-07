82d32ecb1e7a0d355ad693892365e7a53f25264b
unknown <sglen001@fiu.edu>
2022-12-07 06:58:49 -0500

    Working for student view

--- backend.js
+++ backend.js
@@ -17,14 +17,70 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
+app.post('/course/99999/user/10001/notes', (req, res)=>{
+  /* Response (add expected fields for testing)*/
+  var newJson = req.body;
+  newJson.id = 3000;
+  newJson.deleteDate = null;
+  newJson.submitDate = null;
+  newJson.approveDate = null;
+  newJson.returned = false;
+  newJson.feedback = null;
+  newJson.url = "http://localhost:8080/course/99999/70009";
+  res.send(newJson);
+});
+
+app.post('/course/50001/user/10001/notes', (req, res)=>{
+  /* Response (add expected fields for testing)*/
+  var newJson = req.body;
+  newJson.id = 3000;
+  newJson.deleteDate = null;
+  newJson.submitDate = null;
+  newJson.approveDate = null;
+  newJson.returned = false;
+  newJson.feedback = null;
+  newJson.url = "http://localhost:8080/course/50001/70009";
+  res.send(newJson);
+});
+
+app.post('/course/50002/user/10001/notes', (req, res)=>{
+  /* Response (add expected fields for testing)*/
+  var newJson = req.body;
+  newJson.id = 3000;
+  newJson.deleteDate = null;
+  newJson.submitDate = null;
+  newJson.approveDate = null;
+  newJson.returned = false;
+  newJson.feedback = null;
+  newJson.url = "http://localhost:8080/course/50002/70009";
+  res.send(newJson);
+});
+
 app.get('/users/10001/courses', (req, res) => {
   var readStream = fs.readFileSync('./private/user_courses.json');
   res.status(200);
   res.send(readStream);
 });
 
+app.get('/course/50001/user/10001/publishedNotes', (req, res) => {
+  var readStream = fs.readFileSync('./private/published_notes.json');
+  res.status(200);
+  res.send(readStream);
+});
+app.get('/course/50002/user/10001/publishedNotes', (req, res) => {
+  var readStream = fs.readFileSync('./private/published_notes.json');
+  res.status(200);
+  res.send(readStream);
+});
+
 app.get('/course/50001/user/10001/savedNotes', (req, res) => {
   var readStream = fs.readFileSync('./private/saved_notes.json');
+  res.status(200);
+  res.send(readStream);
+});
+
+app.get('/course/99999/user/10001/savedNotes', (req, res) => {
+  var readStream = fs.readFileSync('./private/private_notes.json');
   res.status(200);
   res.send(readStream);
 });
